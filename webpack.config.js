bdd651796a4341c3c9192655903ff6f6943fc2bf
Evan R. Murphy <evanrmurphy@gmail.com>
2014-10-03 09:55:57 -0500

    Undo suitcss/rework setup
    This reverts the following commits:
    - 1bdbe78c40c314cba700a22dd12d67bef13077b1
    - 69082a55bc9c41285c54667bd25a3c4bc42ae8f6
    - 968855ff3ea4089575f99c78e2e620770f2714c2
    - 02be5179a3b1bf5f086c5110cd764b0f30d6a984

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 'use strict'
 
 var path = require('path')
-var reworkLoader = require('rework-loader');
-var reworkVars = require('rework-vars');
-var reworkCalc = require('rework-calc');
-var reworkCustomMedia = require('rework-custom-media');
-var varMap = reworkLoader.makeVarMap('src/index.css');
 
 module.exports =
   { entry: path.join(__dirname, 'src/main.es6.js')
@@ -21,18 +16,7 @@ module.exports =
   , module:
       { loaders:
           [ {test: /\.es6\.js$/, loader: 'webpack-traceur'}
-          , {test: /\.css$/, loader: 'style-loader!rework-loader'}
+          , {test: /\.css$/, loader: 'style!css'}
           ]
-      }
-
-  , rework:
-      { use: 
-        [ reworkLoader.plugins.imports,
-        , reworkLoader.plugins.urls,
-        , reworkLoader.plugins.stripLocalDefs(varMap),
-        , reworkCustomMedia({map: varMap}),
-        , reworkVars({map: varMap}),
-        , reworkCalc
-        ]
       }
   }
